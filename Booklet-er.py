a1e4d731dd3327e3083a30027a02cd8f3c942b97
Kelli Fenelon <44846556+GenomeEditor@users.noreply.github.com>
2022-05-01 20:53:17 -0400

    Add files via upload

--- Booklet-er.py
+++ Booklet-er.py
@@ -1,33 +1,55 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.spatial import ConvexHull
-from PIL import Image
-from pathlib import Path
 import argparse
 import os
-import subprocess
-import re
 from PyPDF3 import PdfFileWriter as pWrite, PdfFileReader
 from PyPDF3.pdf import PageObject
+from decimal import Decimal
 
-def two_merge(page1,page2):
+
+def two_merge(page1,page2,spine_spc):
     total_width = page1.mediaBox.upperRight[0] + page2.mediaBox.upperRight[0]
+    total_width += (Decimal(spine_spc)*total_width)/100
     total_height = max([page1.mediaBox.upperRight[1], page2.mediaBox.upperRight[1]])
+    second_width = float(total_width)-float(page1.mediaBox.upperRight[0])
     new_page = PageObject.createBlankPage(None, total_width, total_height)
     new_page.mergePage(page1)
-    new_page.mergeTranslatedPage(page2, page1.mediaBox.upperRight[0], 0)
+    new_page.mergeTranslatedPage(page2, second_width, 0)
     return new_page
     
-def pdf_split(file,sec_size,out):
-    pdf_in = PdfFileReader(open(file, "rb"), strict=False)
-    num_pages = pdf_in.getNumPages()
+    
+def temp_pdf(pdf, width, height):
+    num_pages = pdf.getNumPages()
+    okay = pWrite()
+    new_page = PageObject.createBlankPage(None, width, height)
+    okay.addPage(new_page)
+    i=0
+    while i < num_pages:
+        page=pdf.getPage(i)
+        okay.addPage(page)
+        i+=1
+    pdf_out = open("temp.pdf", 'wb')
+    okay.write(pdf_out)
+    pdf_out.close()
+    
+    
+def pdf_split(file,sec_size,out, page_shift,spine_spc):
+    pdf_open = PdfFileReader(open(file, "rb"), strict=False)
+    num_pages = pdf_open.getNumPages()
     okay = pWrite()
     if num_pages%sec_size != 0:
         num_pages = num_pages + (sec_size - (num_pages%sec_size))
-    page = pdf_in.getPage(0)
+    page = pdf_open.getPage(0)
     total_width = page.mediaBox.upperRight[0]
     total_height = page.mediaBox.upperRight[1]
     i = 1
+    if page_shift:
+        num_pages+=1
+        temp_pdf(pdf_open,total_width, total_height)
+        close_for_erase = open("temp.pdf", "rb")
+        pdf_in = PdfFileReader(close_for_erase, strict=False)
+        erase = True
+    else:
+        pdf_in = pdf_open
+        erase = False
     j = 1
     while j <= (num_pages/sec_size):
         while i <= sec_size/2:
@@ -49,13 +71,17 @@ def pdf_split(file,sec_size,out):
                     page1 = pdf_in.getPage((i+((j-1)*sec_size))-1)
                 except:
                     page1 = PageObject.createBlankPage(None, total_width, total_height)
-            new_page = two_merge(page1,page2)
+            new_page = two_merge(page1,page2,spine_spc)
             okay.addPage(new_page)
             i+=1
         i=1
         j+=1
     pdf_out = open(out, 'wb')
     okay.write(pdf_out)
+    if erase:
+        close_for_erase.close()
+        os.remove("temp.pdf")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Make Booklet.')
@@ -71,13 +97,26 @@ if __name__ == "__main__":
                                 type = str,
                                 required = False,
                                 help = 'Filename/path for new PDF file.')
+    parser.add_argument('-b', '--blnkpg', dest='page_shift',
+                                action = 'store_const',
+                                const = True, default = False,
+                                help = 'Add a blank page at the start.')
+    parser.add_argument('-d', '--div', dest='spine_spc',
+                                nargs = 1,
+                                default = [0.],
+                                type = float,
+                                required = False,
+                                help = 'Add space in the spine (percentage of width).')
     parser.add_argument('data', type=str, nargs='*', default='./*.pdf',
-						help='PDF File.')
+                                help='PDF File.')
     parse_args = parser.parse_args()
-    datapaths = list(map(Path, parse_args.data))
-    print(datapaths)
     if parse_args.sec_size[0]%4 != 0:
         parse_args.sec_size[0] = 16
         print("Bad section size; defaulting to 16")
-    pdf_split(parse_args.data[0],parse_args.sec_size[0], parse_args.outputfile[:])
+    try:
+        pdf_split(parse_args.data[0],parse_args.sec_size[0], parse_args.outputfile[:],parse_args.page_shift,parse_args.spine_spc[0])
+        print("Completed successfully!")
+    except:
+        print("Something went wrong.")
+        
     
